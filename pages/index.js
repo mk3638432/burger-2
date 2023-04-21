d6d8cf78164339b488fa3e0f1d8afbe055d2bb8d
mk3638432 <mk3638432@gmail.com>
2023-04-21 13:14:22 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import HeadlineCards from '@/Components/HeadlineCards'
 import Hero from '@/Components/Hero'
 import Navbar from '@/Components/Navbar'
 import React from 'react'
+import Head from 'next/head'
 
 const index = () => {
+ 
   return (
     <div>
+      <Head>
+        <title>Restaurants</title>
+      </Head>
       <Navbar/>
       <Hero/>
       <HeadlineCards/>
